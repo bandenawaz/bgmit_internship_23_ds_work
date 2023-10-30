b0ee54a26fba480cec68d27d404d613556438100
Bandenawaz Bagwan <bandenawaz.bagwan.com>
2023-10-30 12:44:31 +0530

    Added more Operators

--- keywords&operators/operators.py
+++ keywords&operators/operators.py
@@ -12,3 +12,53 @@ Operators:
 # +: This is an addition operator used to perform addition 
 # between two variables or values or operands.
 print(5 + 3)
+
+#task: perform -,*,/,//,%,**
+
+'''
+Comparison operators:
+    Comparison operators are used to compare two or more operands
+    or values. It works well with all the data types in Python and
+    returns either True or False.
+'''
+#let define few variables
+a = 5
+b = 3
+x = 5
+y = 8
+
+# == : This is used to compare two variables and values whether they
+# are equal or not.
+print(a == x)
+print(b == y)
+
+#task: perform !=, <,>, <=, >=
+
+'''
+Logical Operators:
+    These operators are used to compare two or more conditional
+    statements or expressions and returns the boolean value
+'''
+#and: This operator is used to compare multiple conditional statements
+#and returns True if all the statements are true, False otherwise.
+print(8 == 8 and a == x)
+
+#task: perform or, not
+
+'''
+Assignment Operator:
+    These operators are used to assign values to the 
+    operands or variables
+'''
+#=: This will assign the value on its right side to the operand
+#on its left side
+f = 9
+g = 8
+
+#+= : This operator adds to the operand on the right side with 
+#the operand on the left side and assigns the result back to same 
+# #operand on the left side
+f += 2
+print(f) 
+
+#task: perform -=,*=,/=,%=,**=,//=
