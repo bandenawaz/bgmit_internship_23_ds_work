af4c09d3d3563dcbbb1f9f46e5f01cfeefab3075
Bandenawaz Bagwan <bandenawaz.bagwan.com>
2023-10-30 12:16:01 +0530

    Added Operattors in Python

---
+++ keywords&operators/operators.py
@@ -0,0 +1,14 @@
+'''
+Operators:
+        Operators are used to perform operations on the variables
+        and values, or operands.
+        These are used to solve an expression.
+
+    List of operators:
+        1. Arithmetic operators:
+            These operator are used with numerical values
+            to perform common mathematical operations
+'''
+# +: This is an addition operator used to perform addition 
+# between two variables or values or operands.
+print(5 + 3)
